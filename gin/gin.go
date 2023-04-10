c3288574be819037777562e8ec902a769b63cb02
seantywork <seantywork@gmail.com>
2023-04-10 22:05:00 +0900

    gin db conn

--- gin/gin.go
+++ gin/gin.go
@@ -4,11 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type healthiness struct {
 	ID     string `json:"id"`
 	Status string `json:"status"`
+}
+
+type user_data struct {
+	DATA []user_record
+}
+
+type user_record struct {
+	UUID    string
+	USER_ID string
+	USER_PW string
+	ACTIVE  int
 }
 
 // albums slice to seed record album data.
@@ -23,6 +38,8 @@ func main() {
 
 	router.POST("/postHealth", postHealth)
 
+	router.GET("/dbCheck", dbCheck)
+
 	router.Run("0.0.0.0:8888")
 }
 
@@ -32,4 +49,38 @@ func getHealth(c *gin.Context) {
 
 func postHealth(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, healthiness_record)
+}
+
+func dbCheck(c *gin.Context) {
+
+	var records user_data
+
+	db, err := sql.Open("mysql", "seantywork:youdonthavetoknow@tcp(127.0.0.1:3306)/chfrank")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM chfrank_user")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var record user_record
+
+		err = results.Scan(&record.UUID, &record.USER_ID, &record.USER_PW, &record.ACTIVE)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		records.DATA = append(records.DATA, record)
+
+	}
+
+	c.IndentedJSON(http.StatusOK, records)
+
 }
--- mysql/init.sql
+++ mysql/init.sql
@@ -7,4 +7,11 @@ CREATE DATABASE chfrank;
 USE chfrank;
 
 
+CREATE TABLE chfrank_user (uuid VARCHAR(150), user_id VARCHAR(50), user_pw VARCHAR(150), ACTIVE TINYINT);
+
+
+
+INSERT INTO chfrank_user(uuid, user_id, user_pw, ACTIVE) values('init_test','init_test','init_test', 1);
+
+
 COMMIT;
