113d3e66e2528cd277605b3757804b1e00b83a53
seantywork <seantywork@gmail.com>
2023-04-11 22:08:27 +0900

    db structure

--- mysql/init.sql
+++ mysql/init.sql
@@ -7,11 +7,15 @@ CREATE DATABASE chfrank;
 USE chfrank;
 
 
-CREATE TABLE chfrank_user (uuid VARCHAR(150), user_id VARCHAR(50), user_pw VARCHAR(150), ACTIVE TINYINT);
+CREATE TABLE chfrank_user (uuid VARCHAR(150), usid VARCHAR(150), user_id VARCHAR(50), user_pw VARCHAR(150), ACTIVE TINYINT);
 
+CREATE TABLE chfrank_channel (cuid VARCHAR(150), channel_passphrase VARCHAR(50), ACTIVE TINYINT);
 
+CREATE TABLE user_in_channel (uuid VARCHAR(150), cuid VARCHAR(150), cidx INT);
 
-INSERT INTO chfrank_user(uuid, user_id, user_pw, ACTIVE) values('init_test','init_test','init_test', 1);
+CREATE TABLE channel_chat_record (cuid VARCHAR(150), cidx INT, uuid VARCHAR(150), record TEXT);
+
+INSERT INTO chfrank_user(uuid, usid, user_id, user_pw, ACTIVE) values('init_test','N','init_test','init_test', 1);
 
 
 COMMIT;
